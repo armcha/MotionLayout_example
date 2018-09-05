f531cdad8bc5ec70d07f08ca29a81ddf8ab7c8d0
Arman Chatikyan <chatikyana@gmail.com>
2018-09-05 12:05:09 +0400

    Added Dark Knight mode :D

--- app/src/main/java/io/armcha/motionlayout/SimpleAdapter.kt
+++ app/src/main/java/io/armcha/motionlayout/SimpleAdapter.kt
@@ -1,18 +1,19 @@
 package io.armcha.motionlayout
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class SimpleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class SimpleAdapter : RecyclerView.Adapter<SimpleAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return object : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)) {}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false))
     }
 
     override fun getItemCount(): Int = 50
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
 
-    }
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 }
